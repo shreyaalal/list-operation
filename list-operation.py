64638bc9e2cabb0a70e9d701bf6a5e3b4c817ef1
Shreya <shreyalal2025@gmail.com>
2023-03-30 18:53:22 +0530

    Add files via upload

---
+++ list-operation.py
@@ -0,0 +1,20 @@
+m1 = [1, 2, 3, 4, 5]
+
+#1. Insert 100 at position 3
+m1.insert(3, 100)
+print(m1)
+
+#2. Delete the number 4
+m1.remove(4)
+print(m1)
+
+#3. Append 200
+m1.append(200)
+print(m1)
+
+#4. Print the list using for loop
+for i in m1:
+    print(i, end=" ")
+
+#5. Delete the list
+del m1
